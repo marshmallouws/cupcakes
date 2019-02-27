2bd1737d36493140e707fd185b27d311cc288361
Marshmallouws <e_hlers@live.dk>
2019-02-27 12:28:10 +0100

    Implemented userclass

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -10,5 +10,33 @@ package data;
  * @author caspe
  */
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private double balance;
+    
+    public User(String username, String password, String email, double balance) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
     
 }
