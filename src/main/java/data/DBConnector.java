ab2da04d3ded0da86386225356b20042fffeff51
Martin <Martin@10.50.137.150>
2019-02-27 12:02:08 +0100

    Added DBConnector

--- src/main/java/data/DBConnector.java
+++ src/main/java/data/DBConnector.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
-/**
- *
- * @author Bitten
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnector {
-    
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/recipes_opgave";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Class.forName(DRIVER);
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                //se hele sekvenst til det gik galt. Dette kan skrives til logfil.
+                ex.printStackTrace();
+            }
+        }
+        return conn;
+   
+    }
 }
