cdb3f47a861c30645b53767bb75ed0180b49601f
Martin <Martin@192.168.0.17>
2019-02-28 14:51:26 +0100

    En masse gode ting: Shopcontroller, bottom, top, dao, shop

---
+++ src/main/java/data/Bottom.java
@@ -0,0 +1,27 @@
+package data;
+
+public class Bottom {
+    private int id;
+    private String name;
+    double price;
+
+    public Bottom(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    
+    
+}
