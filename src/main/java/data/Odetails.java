5fc3e691ab3d99d916bbf124a9ba7579a1a138c0
Vics <Vics@10.50.130.170>
2019-02-28 09:22:10 +0100

    filled order/odetails classes

--- src/main/java/data/Odetails.java
+++ src/main/java/data/Odetails.java
@@ -5,5 +5,46 @@ package data;
  * @author caspe
  */
 public class Odetails {
+    private int order_id = -1;
+    private int top_id;
+    private int bottom_id;
+    private double price;
+    private int qty;
     
+    public Odetails(int top, int bottom, double price, int qty){
+        this.top_id = top;
+        this.bottom_id = bottom;
+        this.price = price;
+        this.qty = qty;
+    }
+    
+    public Odetails(int order, int top, int bottom, double price, int qty){
+        this.order_id = order;
+        this.top_id = top;
+        this.bottom_id = bottom;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public int getOrderID() {
+        return order_id;
+    }
+
+    public int getTopID() {
+        return top_id;
+    }
+
+    public int getBottomID() {
+        return bottom_id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+    
+     
 }
--- src/main/java/data/Order.java
+++ src/main/java/data/Order.java
@@ -5,5 +5,27 @@ package data;
  * @author caspe
  */
 public class Order {
+    private int id;
+    private String username;
+    private String date;
+
+    public Order(int id, String username, String date) {
+        this.id = id;
+        this.username = username;
+        this.date = date;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    
     
 }
